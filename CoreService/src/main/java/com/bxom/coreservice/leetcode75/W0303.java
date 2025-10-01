6728b3d77b3f5fec8d981b69a7b5777a0639bc83
doubleF <lbq0335021@gmail.com>
2025-10-01 15:36:15 +0800

    init W0303

---
+++ CoreService/src/main/java/com/bxom/coreservice/leetcode75/W0303.java
@@ -0,0 +1,21 @@
+package com.bxom.coreservice.leetcode75;
+
+public class W0303 {
+
+    // 试题
+    //
+
+    /**
+     * leetcode-973
+     * https://leetcode.cn/problems/k-closest-points-to-origin/
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("result: " + kClosest(new int[][]{{1, 0}, {0, 1}}, 1));
+    }
+
+    public static int[][] kClosest(int[][] points, int k) {
+        return points;
+    }
+}
