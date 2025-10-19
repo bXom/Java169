bb93bf199d84d31627d753a311106578bcd55a28
doubleF <lbq0335021@gmail.com>
2025-10-19 14:57:03 +0800

    init

--- CoreService/src/main/java/com/bxom/coreservice/leetcode75/W0402.java
+++ CoreService/src/main/java/com/bxom/coreservice/leetcode75/W0402.java
@@ -1,8 +1,12 @@
 package com.bxom.coreservice.leetcode75;
 
 public class W0402 {
-    // 试题
-    // leetcode-208
+    /**
+     * leetcode-208
+     * https://leetcode.cn/problems/implement-trie-prefix-tree/description/
+     *
+     * @param args
+     */
     public static void main(String[] args) {
     }
 }
