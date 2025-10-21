9590d4d34595fd25891134efc245fe67a01d8a84
doubleF <lbq0335021@gmail.com>
2025-10-21 17:19:38 +0800

    init leetcode-1334

---
+++ CoreService/src/main/java/com/bxom/coreservice/minipath/LeetCode1334.java
@@ -0,0 +1,22 @@
+package com.bxom.coreservice.minipath;
+
+public class LeetCode1334 {
+    /**
+     * leetcode-1334
+     * https://leetcode.cn/problems/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/
+     * Floyd 算法
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        findTheCity(1, new int[][]{}, 10);
+    }
+
+    public static int solution1(int n, int[][] edges, int distanceThreshold) {
+        return 1;
+    }
+
+    public static int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        return solution1(n, edges, distanceThreshold);
+    }
+}
