df35886cabd0ecba809d60bf2f3d173f66904682
doubleF <lbq0335021@gmail.com>
2025-10-29 11:37:46 +0800

    finish

--- CoreService/src/main/java/com/bxom/coreservice/sort/Merge.java
+++ CoreService/src/main/java/com/bxom/coreservice/sort/Merge.java
@@ -5,40 +5,40 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Merge {
     public static void main(String[] args) {
-        int[] arr = new int[]{4, 1, 55, 2, 15, 6, 74, 2, 5, 4, 5, 8888, 25, 64};
+//        int[] arr = new int[]{4, 1, 55, 2, 15, 6, 74, 2, 5, 4, 5, 8888, 25, 64};
+        int[] arr = new int[]{3, 1, 2};
         sort(arr);
         log.info("result: {}", arr);
     }
 
     private static void sort(int[] arr) {
         int size = arr.length;
+        // 待归并的两个集合元素步长,初始值为 1,依次翻倍
         int step = 1;
-        while (2 * step < size) {
+        while (size >= step) {
+            // 单次归并的两个集合的指针
             for (int index = 0; index < size; ) {
+                // 归并后得到的集合
                 int[] sorted = new int[2 * step];
+                // 子集合指针
                 int left = index;
                 int right = index + step;
-                log.info("step: {}, index: {}, left: {}, right: {}", step, index, left, right);
-                for (int i = 0; i < 2 * step; i++) {
-                    log.info("arr[left] - arr[{}]: {}", left, arr[left]);
-                    log.info("arr[right] - arr[{}]: {}", right, arr[right]);
-                    if (left < index + step && right < index + step * 2) {
+                for (int i = 0; (i < 2 * step); i++) {
+                    if (left < index + step && right < index + step * 2 && left < size && right < size) {
                         if (arr[left] < arr[right]) {
                             sorted[i] = arr[left++];
                         } else {
                             sorted[i] = arr[right++];
                         }
-                    } else if (left < index + step) {
+                    } else if (left < index + step && left < size) {
                         sorted[i] = arr[left++];
-                    } else if (right < index + 2 * step) {
+                    } else if (right < index + 2 * step && right < size) {
                         sorted[i] = arr[right++];
                     }
-                    log.info("sorted - i: {}, val: {}", i, sorted);
                 }
-                log.info("sorted: {}", sorted);
                 // 覆盖原集合
                 for (int i = 0; i < 2 * step; i++) {
-                    arr[index + i] = sorted[i];
+                    if (index + i < size) arr[index + i] = sorted[i];
                 }
                 index += 2 * step;
             }
