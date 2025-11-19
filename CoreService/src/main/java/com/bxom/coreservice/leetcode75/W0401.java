34dfcbdc4f7b4b815555b30611703e71a064f6da
doubleF <lbq0335021@gmail.com>
2025-11-19 17:33:45 +0800

    rewrite

--- CoreService/src/main/java/com/bxom/coreservice/leetcode75/W0401.java
+++ CoreService/src/main/java/com/bxom/coreservice/leetcode75/W0401.java
@@ -15,50 +15,19 @@ public class W0401 {
 
     // TODO: failed
     private static boolean solution1(int numCourses, int[][] prerequisites) {
-//        if (prerequisites.length == 0) return true;
-        Map<Integer, Set<Integer>> preMap = new HashMap<>();
-        for (int[] cond : prerequisites) {
-//            if (cond[0] == cond[1]) return false;
-            if (!preMap.containsKey(cond[0])) {
-                preMap.put(cond[0], new HashSet<>());
-            }
-            if (!preMap.containsKey(cond[1])) {
-                preMap.put(cond[1], new HashSet<>());
-            }
-            preMap.get(cond[0]).add(cond[1]);
-            Queue<Integer> checkQueue = new LinkedList<>();
-            checkQueue.add(cond[0]);
-            if (!reloadPre(preMap, checkQueue)) return false;
-//            preMap.forEach((k, vList) -> {
-//                StringBuilder sb = new StringBuilder();
-//                vList.forEach(v -> sb.append(v).append(","));
-//                log.info("{}: {}", k, sb);
-//            });
-//            log.info("----------------");
+        Map<Integer, Integer> intoMap = new HashMap<>();
+        for (int[] item : prerequisites) {
+            intoMap.put(item[0], intoMap.getOrDefault(item[0], 0) + 1);
         }
-        return preMap.size() <= numCourses;
-    }
+        List<Integer> sortedList = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> item : intoMap.entrySet()) {
+            int times = item.getValue();
+            int name = item.getKey();
+            if (times == 0) {
 
-    private static boolean reloadPre(Map<Integer, Set<Integer>> preMap, Queue<Integer> checkQueue) {
-        while (!checkQueue.isEmpty()) {
-            Integer key = checkQueue.poll();
-//            log.info("key: {}, remainSIze: {}", key, checkQueue.size());
-            preMap.forEach((k, vList) -> {
-                Set<Integer> newSet = new HashSet<>();
-                vList.forEach(v -> {
-                    if (v.equals(key)) {
-                        newSet.addAll(preMap.get(key));
-                        if (newSet.contains(k)) return;
-                        checkQueue.add(k);
-                    }
-                });
-                preMap.get(k).addAll(newSet);
-            });
+            }
         }
-        for (Map.Entry<Integer, Set<Integer>> entry : preMap.entrySet()) {
-            if (entry.getValue().contains(entry.getKey())) return false;
-        }
-        return true;
+        return false;
     }
 
     public static boolean canFinish(int numCourses, int[][] prerequisites) {
