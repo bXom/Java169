0a6bcfa9d21c923698269744baf0780baf20be60
doubleF <lbq0335021@gmail.com>
2025-11-20 17:24:04 +0800

    init

--- CoreService/src/main/java/com/bxom/coreservice/leetcode75/W0403.java
+++ CoreService/src/main/java/com/bxom/coreservice/leetcode75/W0403.java
@@ -1,8 +1,26 @@
 package com.bxom.coreservice.leetcode75;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
+
+@Slf4j
 public class W0403 {
-    // 试题
-    // leetcode-322
+    /**
+     * leetcode-322
+     * https://leetcode.cn/problems/coin-change/
+     *
+     * @param args
+     */
     public static void main(String[] args) {
+        log.info("{}", coinChange(new int[]{1, 2, 5}, 11));
+    }
+
+    public static int solution(int[] coins, int amount) {
+        return -1;
+    }
+
+    public static int coinChange(int[] coins, int amount) {
+        return solution(coins, amount);
     }
 }
