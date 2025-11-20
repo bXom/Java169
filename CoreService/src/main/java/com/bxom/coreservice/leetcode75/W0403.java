533a3126c13f031974f1f2ec8147213f6810b230
ff@mbpm5 <emforffsc@outlook.com>
2025-11-20 22:26:00 +0800

    finish

--- CoreService/src/main/java/com/bxom/coreservice/leetcode75/W0403.java
+++ CoreService/src/main/java/com/bxom/coreservice/leetcode75/W0403.java
@@ -17,7 +17,21 @@ public class W0403 {
     }
 
     public static int solution(int[] coins, int amount) {
-        return -1;
+        Arrays.sort(coins);
+        // 金额从 0 到 amount
+        int[] amountArr = new int[amount + 1];
+        // 最大金额的最大硬币数量 amount + 1
+        int max = amount + 1;
+        Arrays.fill(amountArr, max);
+        amountArr[0] = 0;
+        for (int i = 1; i < amount + 1; i++) {
+            for (int coin : coins) {
+                if (coin <= i) {
+                    amountArr[i] = Math.min(amountArr[i], amountArr[i - coin] + 1);
+                }
+            }
+        }
+        return amountArr[amount] == amount + 1 ? -1 : amountArr[amount];
     }
 
     public static int coinChange(int[] coins, int amount) {
