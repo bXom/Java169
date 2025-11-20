3da212713407d5a1a41ebcf47b8f79f0f6f27c9f
doubleF <lbq0335021@gmail.com>
2025-11-20 11:26:25 +0800

    init

--- CoreService/src/main/java/com/bxom/coreservice/leetcode75/W0402.java
+++ CoreService/src/main/java/com/bxom/coreservice/leetcode75/W0402.java
@@ -1,5 +1,8 @@
 package com.bxom.coreservice.leetcode75;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class W0402 {
     /**
      * leetcode-208
@@ -8,5 +11,29 @@ public class W0402 {
      * @param args
      */
     public static void main(String[] args) {
+        Trie trie = new Trie();
+        trie.insert("apple");
+    }
+
+    static class Trie {
+        private List<String> wordList;
+        private List<String> prefixList;
+
+        Trie() {
+            this.wordList = new ArrayList<>();
+            this.prefixList = new ArrayList<>();
+        }
+
+        public void insert(String word) {
+
+        }
+
+        public boolean search(String word) {
+            return false;
+        }
+
+        public boolean startsWith(String prefix) {
+            return false;
+        }
     }
 }
