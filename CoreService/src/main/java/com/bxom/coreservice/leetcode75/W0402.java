1e191b5cb97d5136fe7fda74583287cec7c74dfc
doubleF <lbq0335021@gmail.com>
2025-11-20 13:31:16 +0800

    finish

--- CoreService/src/main/java/com/bxom/coreservice/leetcode75/W0402.java
+++ CoreService/src/main/java/com/bxom/coreservice/leetcode75/W0402.java
@@ -1,8 +1,12 @@
 package com.bxom.coreservice.leetcode75;
 
-import java.util.ArrayList;
-import java.util.List;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.HashSet;
+import java.util.Set;
+
+@Slf4j
 public class W0402 {
     /**
      * leetcode-208
@@ -13,27 +17,38 @@ public class W0402 {
     public static void main(String[] args) {
         Trie trie = new Trie();
         trie.insert("apple");
+        log.info("words: ------------");
+        trie.getWordsSet().forEach(log::info);
+        log.info("prefix: ------------");
+        trie.getPrefixSet().forEach(log::info);
     }
 
+    @Data
     static class Trie {
-        private List<String> wordList;
-        private List<String> prefixList;
+        private Set<String> wordsSet;
+        private Set<String> prefixSet;
 
         Trie() {
-            this.wordList = new ArrayList<>();
-            this.prefixList = new ArrayList<>();
+            this.wordsSet = new HashSet<>();
+            this.prefixSet = new HashSet<>();
         }
 
         public void insert(String word) {
-
+            this.wordsSet.add(word);
+            int length = word.length();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < length; i++) {
+                sb.append(word.charAt(i));
+                this.prefixSet.add(sb.toString());
+            }
         }
 
         public boolean search(String word) {
-            return false;
+            return this.wordsSet.contains(word);
         }
 
         public boolean startsWith(String prefix) {
-            return false;
+            return this.prefixSet.contains(prefix);
         }
     }
 }
