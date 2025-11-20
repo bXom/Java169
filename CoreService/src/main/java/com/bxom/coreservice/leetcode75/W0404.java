5cf6f5dbe594eecf0be2d7d3e8884fd0044e8151
ff@mbpm5 <emforffsc@outlook.com>
2025-11-20 22:33:06 +0800

    init

--- CoreService/src/main/java/com/bxom/coreservice/leetcode75/W0404.java
+++ CoreService/src/main/java/com/bxom/coreservice/leetcode75/W0404.java
@@ -1,8 +1,24 @@
 package com.bxom.coreservice.leetcode75;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class W0404 {
-    // 试题
-    // leetcode-238
+    /**
+     * leetcode-238
+     * https://leetcode.cn/problems/product-of-array-except-self/
+     *
+     * @param args
+     */
     public static void main(String[] args) {
+        log.info("result: {}", productExceptSelf(new int[]{1, 2}));
+    }
+
+    public static int[] solution(int[] nums) {
+        return nums;
+    }
+
+    public static int[] productExceptSelf(int[] nums) {
+        return solution(nums);
     }
 }
