066c57513dfc7af60d7d56045e8dea5f4af02b6d
doubleF <lbq0335021@gmail.com>
2025-11-20 14:09:44 +0800

    in correct tree way

--- CoreService/src/main/java/com/bxom/coreservice/leetcode75/W0402.java
+++ CoreService/src/main/java/com/bxom/coreservice/leetcode75/W0402.java
@@ -17,38 +17,99 @@ public class W0402 {
     public static void main(String[] args) {
         Trie trie = new Trie();
         trie.insert("apple");
-        log.info("words: ------------");
-        trie.getWordsSet().forEach(log::info);
-        log.info("prefix: ------------");
-        trie.getPrefixSet().forEach(log::info);
+        Trie[] children = trie.children;
+        boolean isEnd = trie.isEnd;
+        while (!isEnd) {
+            for (int i = 0; i < 26; i++) {
+                if (children[i] != null) log.info("{}", Character.toString(97 + i));
+            }
+            for (Trie child : children) {
+                if (child != null) {
+                    children = child.children;
+                    isEnd = child.isEnd;
+                }
+            }
+        }
+
+//        Trie trie = new Trie();
+//        trie.insert("apple");
+//        log.info("words: ------------");
+//        trie.getWordsSet().forEach(log::info);
+//        log.info("prefix: ------------");
+//        trie.getPrefixSet().forEach(log::info);
     }
 
     @Data
     static class Trie {
-        private Set<String> wordsSet;
-        private Set<String> prefixSet;
+        private Trie[] children;
+        private boolean isEnd;
 
         Trie() {
-            this.wordsSet = new HashSet<>();
-            this.prefixSet = new HashSet<>();
+            this.children = new Trie[26];
+            this.isEnd = false;
         }
 
         public void insert(String word) {
-            this.wordsSet.add(word);
             int length = word.length();
-            StringBuilder sb = new StringBuilder();
+            Trie node = this;
             for (int i = 0; i < length; i++) {
-                sb.append(word.charAt(i));
-                this.prefixSet.add(sb.toString());
+                int index = word.charAt(i) - 'a';
+                if (node.children[index] == null) {
+                    node.children[index] = new Trie();
+                }
+                node = node.children[index];
             }
+            node.isEnd = true;
         }
 
         public boolean search(String word) {
-            return this.wordsSet.contains(word);
+            Trie node = searchPrefix(word);
+            return node != null && node.isEnd;
         }
 
         public boolean startsWith(String prefix) {
-            return this.prefixSet.contains(prefix);
+            return searchPrefix(prefix) != null;
+        }
+
+        private Trie searchPrefix(String word) {
+            Trie node = this;
+            int length = word.length();
+            for (int i = 0; i < length; i++) {
+                int index = word.charAt(i) - 'a';
+                if (node.children[index] == null) {
+                    return null;
+                }
+                node = node.children[index];
+            }
+            return node;
         }
     }
+//    @Data
+//    static class Trie {
+//        private Set<String> wordsSet;
+//        private Set<String> prefixSet;
+//
+//        Trie() {
+//            this.wordsSet = new HashSet<>();
+//            this.prefixSet = new HashSet<>();
+//        }
+//
+//        public void insert(String word) {
+//            this.wordsSet.add(word);
+//            int length = word.length();
+//            StringBuilder sb = new StringBuilder();
+//            for (int i = 0; i < length; i++) {
+//                sb.append(word.charAt(i));
+//                this.prefixSet.add(sb.toString());
+//            }
+//        }
+//
+//        public boolean search(String word) {
+//            return this.wordsSet.contains(word);
+//        }
+//
+//        public boolean startsWith(String prefix) {
+//            return this.prefixSet.contains(prefix);
+//        }
+//    }
 }
