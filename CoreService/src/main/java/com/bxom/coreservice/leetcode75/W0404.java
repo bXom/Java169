eb0c9cafeb278b2f81162f75e56fb30b92110755
doubleF <lbq0335021@gmail.com>
2025-11-21 11:21:45 +0800

    complete solution2

--- CoreService/src/main/java/com/bxom/coreservice/leetcode75/W0404.java
+++ CoreService/src/main/java/com/bxom/coreservice/leetcode75/W0404.java
@@ -14,6 +14,20 @@ public class W0404 {
         log.info("result: {}", productExceptSelf(new int[]{1, 2, 3, 4, 5}));
     }
 
+    public static int[] solution2(int[] nums) {
+        int[] result = new int[nums.length];
+        result[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            result[i] = nums[i - 1] * result[i - 1];
+        }
+        int rightVal = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            result[i] *= rightVal;
+            rightVal *= nums[i];
+        }
+        return result;
+    }
+
     public static int[] solution(int[] nums) {
         int[] left = new int[nums.length];
         left[0] = 1;
@@ -33,6 +47,7 @@ public class W0404 {
     }
 
     public static int[] productExceptSelf(int[] nums) {
-        return solution(nums);
+        return solution2(nums);
+//        return solution(nums);
     }
 }
