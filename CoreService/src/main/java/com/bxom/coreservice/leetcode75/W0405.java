36637b8a6b4730ef52d496b376db7086ab7869be
doubleF <lbq0335021@gmail.com>
2025-11-21 11:23:57 +0800

    init

--- CoreService/src/main/java/com/bxom/coreservice/leetcode75/W0405.java
+++ CoreService/src/main/java/com/bxom/coreservice/leetcode75/W0405.java
@@ -1,8 +1,37 @@
 package com.bxom.coreservice.leetcode75;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class W0405 {
-    // 试题
-    // leetcode-155
+    /**
+     * leetcode-155
+     * https://leetcode.cn/problems/min-stack/
+     *
+     * @param args
+     */
     public static void main(String[] args) {
+    }
+
+    static class MinStack {
+
+        MinStack() {
+        }
+
+        public void push(int val) {
+
+        }
+
+        public void pop() {
+
+        }
+
+        public int top() {
+            return -1;
+        }
+
+        public int getMin() {
+            return -1;
+        }
     }
 }
