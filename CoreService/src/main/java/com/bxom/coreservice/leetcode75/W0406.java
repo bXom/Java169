d75c900d12dc8db913621df095b389f91bfdd0b5
doubleF <lbq0335021@gmail.com>
2025-11-21 17:18:27 +0800

    init

--- CoreService/src/main/java/com/bxom/coreservice/leetcode75/W0406.java
+++ CoreService/src/main/java/com/bxom/coreservice/leetcode75/W0406.java
@@ -1,8 +1,43 @@
 package com.bxom.coreservice.leetcode75;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class W0406 {
-    // 试题
-    // leetcode-98
+    /**
+     * leetcode-98
+     * https://leetcode.cn/problems/validate-binary-search-tree/
+     *
+     * @param args
+     */
     public static void main(String[] args) {
+        log.info("result: {}", isValidBST(new TreeNode()));
+    }
+
+    public static boolean solution(TreeNode root) {
+        return false;
+    }
+
+    public static boolean isValidBST(TreeNode root) {
+        return solution(root);
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
     }
 }
