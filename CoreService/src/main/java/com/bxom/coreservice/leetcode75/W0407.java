0bda10a2536e5fe182703bf474c880842052fa52
ff@mbpm5 <emforffsc@outlook.com>
2025-11-22 11:54:48 +0800

    init

--- CoreService/src/main/java/com/bxom/coreservice/leetcode75/W0407.java
+++ CoreService/src/main/java/com/bxom/coreservice/leetcode75/W0407.java
@@ -1,8 +1,24 @@
 package com.bxom.coreservice.leetcode75;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class W0407 {
-    // 试题
-    // leetcode-200
+    /**
+     * leetcode-200
+     * https://leetcode.cn/problems/number-of-islands/description/
+     *
+     * @param args
+     */
     public static void main(String[] args) {
+        log.info("result: {}", numIslands(new char[][]{}));
+    }
+
+    public static int solution(char[][] grid) {
+        return 0;
+    }
+
+    public static int numIslands(char[][] grid) {
+        return solution(grid);
     }
 }
