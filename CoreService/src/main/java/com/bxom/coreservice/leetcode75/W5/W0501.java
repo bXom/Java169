465cfef5e9569578ba5a01ca302bc9dafc047c55
doubleF <lbq0335021@gmail.com>
2025-11-24 10:39:41 +0800

    init

--- CoreService/src/main/java/com/bxom/coreservice/leetcode75/W5/W0501.java
+++ CoreService/src/main/java/com/bxom/coreservice/leetcode75/W5/W0501.java
@@ -1,8 +1,24 @@
 package com.bxom.coreservice.leetcode75.W5;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class W0501 {
-    // 试题
-    // leetcode-33
+    /**
+     * leetcode-33
+     * https://leetcode.cn/problems/search-in-rotated-sorted-array/description/
+     *
+     * @param args
+     */
     public static void main(String[] args) {
+        log.info("result: {}", search(new int[]{3, 5, 92, 1, 952, 34}, 1));
+    }
+
+    public static int solution(int[] nums, int target) {
+        return target;
+    }
+
+    public static int search(int[] nums, int target) {
+        return solution(nums, target);
     }
 }
