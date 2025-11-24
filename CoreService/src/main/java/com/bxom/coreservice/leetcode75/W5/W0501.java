5c831c7c688e1b45a3044371aa2feca07c2293d8
doubleF <lbq0335021@gmail.com>
2025-11-24 11:34:11 +0800

    upload

--- CoreService/src/main/java/com/bxom/coreservice/leetcode75/W5/W0501.java
+++ CoreService/src/main/java/com/bxom/coreservice/leetcode75/W5/W0501.java
@@ -15,6 +15,32 @@ public class W0501 {
     }
 
     public static int solution(int[] nums, int target) {
+        int length = nums.length;
+        int left = 0;
+        int right = length - 1;
+        while (left <= right) {
+            int mid = (right - left) / 2;
+            if (nums[left] == target) return left;
+            if (nums[right] == target) return right;
+            if (nums[mid] == target) return mid;
+//            0
+//            6,0,1,2,3,4,5
+//            2,3,4,5,6,0,1
+            if (nums[left] > nums[mid]) {
+                if (target < nums[mid]) {
+                    right = mid - 1;
+                    left++;
+                } else if (target > nums[left]) {
+                    right = mid;
+                    left++;
+                } else {
+                    left = mid + 1;
+                    right--;
+                }
+            } else {
+
+            }
+        }
         return target;
     }
 
