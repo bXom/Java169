cfc4e702fdb532a4e789fdbb2463f495f71dd557
doubleF <lbq0335021@gmail.com>
2025-11-24 16:41:10 +0800

    init

--- CoreService/src/main/java/com/bxom/coreservice/leetcode75/W5/W0503.java
+++ CoreService/src/main/java/com/bxom/coreservice/leetcode75/W5/W0503.java
@@ -1,8 +1,28 @@
 package com.bxom.coreservice.leetcode75.W5;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
 public class W0503 {
-    // 试题
-    // leetcode-46
+    /**
+     * leetcode-46
+     * https://leetcode.cn/problems/permutations/description/
+     *
+     * @param args
+     */
     public static void main(String[] args) {
+        log.info("result: {}", permute(new int[]{1, 2, 5}));
+    }
+
+    public static List<List<Integer>> solution(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        return result;
+    }
+
+    public static List<List<Integer>> permute(int[] nums) {
+        return solution(nums);
     }
 }
