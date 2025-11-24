f6ef7dd109b8e184d7ccd64b0f3c7bc07b58275b
doubleF <lbq0335021@gmail.com>
2025-11-24 14:07:46 +0800

    init

--- CoreService/src/main/java/com/bxom/coreservice/leetcode75/W5/W0502.java
+++ CoreService/src/main/java/com/bxom/coreservice/leetcode75/W5/W0502.java
@@ -1,8 +1,28 @@
 package com.bxom.coreservice.leetcode75.W5;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
 public class W0502 {
-    // 试题
-    // leetcode-39
+    /**
+     * leetcode-39
+     * https://leetcode.cn/problems/combination-sum/description/
+     *
+     * @param args
+     */
     public static void main(String[] args) {
+        log.info("result: {}", combinationSum(new int[]{2, 3, 6, 7}, 7));
+    }
+
+    public static List<List<Integer>> solution(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        return result;
+    }
+
+    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+        return solution(candidates, target);
     }
 }
