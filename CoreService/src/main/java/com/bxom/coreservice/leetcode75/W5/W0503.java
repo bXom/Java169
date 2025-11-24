650b23a81e544a3480a6dc359edbc850c64bbc20
ff@mbpm5 <emforffsc@outlook.com>
2025-11-24 22:35:20 +0800

    upload demo

--- CoreService/src/main/java/com/bxom/coreservice/leetcode75/W5/W0503.java
+++ CoreService/src/main/java/com/bxom/coreservice/leetcode75/W5/W0503.java
@@ -18,8 +18,35 @@ public class W0503 {
     }
 
     public static List<List<Integer>> solution(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        return result;
+        List<List<Integer>> res = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+        backtrack(nums, new ArrayList<>(), used, res);
+        return res;
+    }
+
+    private static void backtrack(int[] nums, List<Integer> path, boolean[] used, List<List<Integer>> res) {
+        // 结束条件
+        if (path.size() == nums.length) {
+            res.add(new ArrayList<>(path)); // 必须new一个新的ArrayList
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) {
+                continue;
+            }
+
+            // 做选择
+            path.add(nums[i]);
+            used[i] = true;
+
+            // 递归
+            backtrack(nums, path, used, res);
+
+            // 撤销选择
+            path.remove(path.size() - 1);
+            used[i] = false;
+        }
     }
 
     public static List<List<Integer>> permute(int[] nums) {
