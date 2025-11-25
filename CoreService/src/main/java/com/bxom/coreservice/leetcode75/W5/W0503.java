5b027a5cb4c15f319573d7b6365f26cbe0314a85
ff@mbpm5 <emforffsc@outlook.com>
2025-11-25 22:12:18 +0800

    complete

--- CoreService/src/main/java/com/bxom/coreservice/leetcode75/W5/W0503.java
+++ CoreService/src/main/java/com/bxom/coreservice/leetcode75/W5/W0503.java
@@ -19,7 +19,26 @@ public class W0503 {
 
     public static List<List<Integer>> solution(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
+        backtracking(nums, new boolean[nums.length], new ArrayList<>(), res);
         return res;
+    }
+
+    private static void backtracking(int[] nums, boolean[] used, List<Integer> path, List<List<Integer>> res) {
+        int length = nums.length;
+        if (path.size() == length) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = 0; i < length; i++) {
+            if (used[i]) {
+                continue;
+            }
+            path.add(nums[i]);
+            used[i] = true;
+            backtracking(nums, used, path, res);
+            path.remove(path.size() - 1);
+            used[i] = false;
+        }
     }
 
     public static List<List<Integer>> permute(int[] nums) {
