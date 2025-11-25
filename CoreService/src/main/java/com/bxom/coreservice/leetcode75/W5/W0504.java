aac10fe89fc4d30e0d55d84ba4f9ad9a52e3e298
doubleF <lbq0335021@gmail.com>
2025-11-25 17:28:16 +0800

    init

--- CoreService/src/main/java/com/bxom/coreservice/leetcode75/W5/W0504.java
+++ CoreService/src/main/java/com/bxom/coreservice/leetcode75/W5/W0504.java
@@ -1,8 +1,24 @@
 package com.bxom.coreservice.leetcode75.W5;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class W0504 {
-    // 试题
-    // leetcode-56
+    /**
+     * leetcode-56
+     * https://leetcode.cn/problems/merge-intervals/description/
+     *
+     * @param args
+     */
     public static void main(String[] args) {
+        log.info("result: {}", merge(new int[][]{}));
+    }
+
+    public static int[][] solution(int[][] intervals) {
+        return intervals;
+    }
+
+    public static int[][] merge(int[][] intervals) {
+        return solution(intervals);
     }
 }
