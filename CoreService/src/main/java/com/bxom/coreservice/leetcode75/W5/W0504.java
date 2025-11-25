43001aaafec863b0be4aa9c299358dfef4dc61b2
ff@mbpm5 <emforffsc@outlook.com>
2025-11-25 22:34:58 +0800

    init

--- CoreService/src/main/java/com/bxom/coreservice/leetcode75/W5/W0504.java
+++ CoreService/src/main/java/com/bxom/coreservice/leetcode75/W5/W0504.java
@@ -2,6 +2,8 @@ package com.bxom.coreservice.leetcode75.W5;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Arrays;
+
 @Slf4j
 public class W0504 {
     /**
@@ -11,10 +13,30 @@ public class W0504 {
      * @param args
      */
     public static void main(String[] args) {
-        log.info("result: {}", merge(new int[][]{}));
+        log.info("result: {}", merge(new int[][]{{1, 3}, {15, 18}, {8, 10}, {2, 6}}));
     }
 
     public static int[][] solution(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        int index = 0;
+        while (index < intervals.length - 1) {
+            int ahead0 = intervals[index][0];
+            int ahead1 = intervals[index][1];
+            int behind0 = intervals[index + 1][0];
+            int behind1 = intervals[index + 1][1];
+            int[] newInt = new int[]{ahead0, 0};
+            if (behind0 <= ahead1) {
+                if (behind1 <= ahead1) {
+                    newInt[1] = ahead1;
+                } else {
+                    newInt[1] = behind1;
+                }
+//                intervals[index + 1] = newInt;
+//                intervals.remove[index];
+            } else {
+                index++;
+            }
+        }
         return intervals;
     }
 
