816ceecbd1f343b177afa390907c11ec718768e5
doubleF <lbq0335021@gmail.com>
2025-12-09 11:15:55 +0800

    complete

--- CoreService/src/main/java/com/bxom/coreservice/leetcode75/W5/W0506.java
+++ CoreService/src/main/java/com/bxom/coreservice/leetcode75/W5/W0506.java
@@ -1,8 +1,8 @@
 package com.bxom.coreservice.leetcode75.W5;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.TreeMap;
 
 public class W0506 {
     /**
@@ -15,31 +15,40 @@ public class W0506 {
     }
 
     class TimeMap {
-        HashMap<String, TreeMap<Integer, String>> map;
+        HashMap<String, ArrayList<HashMap<Integer, String>>> map;
 
         public TimeMap() {
             this.map = new HashMap<>();
         }
 
         public void set(String key, String value, int timestamp) {
-            if (!map.containsKey(key)) {
-                map.put(key, new TreeMap<>());
-            }
-            map.get(key).put(timestamp, value);
+            map.computeIfAbsent(key, k -> new ArrayList<>());
+            HashMap<Integer, String> kv = new HashMap<>();
+            kv.put(timestamp, value);
+            map.get(key).add(kv);
         }
 
         public String get(String key, int timestamp) {
             if (!map.containsKey(key)) return "";
-            Map<Integer, String> keyMap = map.get(key);
-            if (keyMap.containsKey(timestamp)) return keyMap.get(timestamp);
-            int start = 1;
-            int end = timestamp - 1;
+            ArrayList<HashMap<Integer, String>> keyMap = map.get(key);
+            int start = 0;
+            int end = keyMap.size() - 1;
+            String result = "";
             while (start <= end) {
                 int mid = start + (end - start) / 2;
-                if (keyMap.containsKey(mid)) start = mid + 1;
-                else end = mid - 1;
+                HashMap<Integer, String> map = keyMap.get(mid);
+                for (Map.Entry<Integer, String> kv : map.entrySet()) {
+                    int ts = kv.getKey();
+                    String value = kv.getValue();
+                    if (ts <= timestamp) {
+                        result = value;
+                        start = mid + 1;
+                    } else {
+                        end = mid - 1;
+                    }
+                }
             }
-            return "";
+            return result;
         }
     }
 }
